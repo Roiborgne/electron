c432072265785a9617a3a99c6120422acca908ac
hamilcaron <hamilcaronolan@gmail.com>
2025-03-11 14:55:22 +0100

    tuto-4

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 
 // Processus Principal
 
-const {app, BrowserWindow, ipcMain} = require("electron")
+const {app, BrowserWindow, ipcMain, Menu} = require("electron")
 const path = require("path")
 const process = require("node:process");
 
+let window;
 //Créer la fenêtre principale
 
 function createwindow() {
@@ -19,7 +20,48 @@ function createwindow() {
             preload: path.join(__dirname, 'src/js/preload.js')
         }
     })
+    //Créer le menu
+    createMenu()
     window.loadFile('src/pages/index.html');
+}
+
+//Fonction permettant de créer un menu personalisé
+function createMenu() {
+    //Créer un modèle qui va représenter le menu -> modèle
+    const template = [
+        {
+            label : "App",
+            submenu: [
+                {
+                    label : "Versions",
+                    click : () => window.loadFile("src/pages/index.html")
+                },
+                {
+                    label : "Quitter",
+                    accelerator: process.platform === "Darwin" ? "Cmd+Q" : "CTRl + Q",
+                    click : () => app.quit()
+                }
+                ]
+        },
+        {
+            label : "Tâches",
+            submenu: [
+                {
+                    label : "Lister",
+                    click : () => window.loadFile("src/pages/listes-taches.html")
+                },
+                {
+                    label : "Ajouter",
+                    click : () => window.loadFile("src/pages/ajouter.html")
+                }
+                ]
+        }
+    ]
+    //Créer le menu à partir du modèle
+    const menu = Menu.buildFromTemplate(template)
+
+    //Définir le menu comme étant le menu de l'application
+    Menu.setApplicationMenu(menu)
 }
 
 //initialisation de l'application au démarage
